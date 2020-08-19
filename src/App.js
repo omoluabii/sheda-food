e50b6be58af6778bf41a09345cb343219e29e357
MynameisRita <66781954+MynameisRita@users.noreply.github.com>
2020-08-19 15:41:07 +0100

    Sheda-food

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div></div>
+    <>
+    <Navbar/>
+    </>
   );
 }
 
