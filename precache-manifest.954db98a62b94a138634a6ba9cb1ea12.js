f6f29a2cdb82c3854f78f39d884c055321b812de
omoluabii <hakra911@gmail.com>
2020-08-27 13:43:23 +0100

    Updates

---
+++ precache-manifest.954db98a62b94a138634a6ba9cb1ea12.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdb7589d1f49056616ea99d7e3ba9e4",
+    "url": "/sheda-food/index.html"
+  },
+  {
+    "revision": "4edc3f2f63074953bc54",
+    "url": "/sheda-food/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "c51e8b0858fd318c24f8",
+    "url": "/sheda-food/static/css/main.4862173b.chunk.css"
+  },
+  {
+    "revision": "4edc3f2f63074953bc54",
+    "url": "/sheda-food/static/js/2.ae40181c.chunk.js"
+  },
+  {
+    "revision": "4d833975c154ea21126df2a4e5867037",
+    "url": "/sheda-food/static/js/2.ae40181c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51e8b0858fd318c24f8",
+    "url": "/sheda-food/static/js/main.bd4f0137.chunk.js"
+  },
+  {
+    "revision": "47c2d26aefcb8dde9d2a",
+    "url": "/sheda-food/static/js/runtime-main.192d3ea1.js"
+  },
+  {
+    "revision": "f390c60c9437ac12106d44497d2f09c6",
+    "url": "/sheda-food/static/media/food1.f390c60c.png"
+  },
+  {
+    "revision": "5ac1f2eb158966350b0476fe123d8d70",
+    "url": "/sheda-food/static/media/hero_2.5ac1f2eb.jpg"
+  },
+  {
+    "revision": "fe81e25da55c878ec30477aaa7c0924e",
+    "url": "/sheda-food/static/media/logo1.fe81e25d.png"
+  }
+]);
